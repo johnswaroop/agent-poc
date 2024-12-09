d6abcfefa14f5d13b91f09a6601eb3d81f6b573e
“John <johnswaroop28@gmail.com>
2024-12-09 17:51:53 +0530

    agent poc

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { VoiceAssistantComponent } from "../components/voice-assistant";
+
+export default function Home() {
+  return (
+    <div className="flex w-full h-full items-center justify-center">
+      <VoiceAssistantComponent />
+    </div>
+  );
+}
